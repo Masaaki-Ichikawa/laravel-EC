a79cd2801b862faf3f17f25626522125241a8515
Masaaki-Ichikawa <matyaakitabasko@gmail.com>
2023-08-18 17:41:08 +0900

    リサイズ無しアップロード

--- app/Http/Controllers/Owner/ShopController.php
+++ app/Http/Controllers/Owner/ShopController.php
@@ -7,6 +7,7 @@ use App\Models\Shop;
 use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
 
 class ShopController extends Controller
 {
@@ -41,7 +42,9 @@ class ShopController extends Controller
      */
     public function edit(string $id)
     {
-        dd(Shop::findOrFail($id));
+        $shop = Shop::findOrFail($id);
+        // dd(Shop::findOrFail($id));
+        return view('owner.shops.edit', compact('shop'));
     }
 
     /**
@@ -49,6 +52,11 @@ class ShopController extends Controller
      */
     public function update(Request $request, string $id)
     {
+        $imageFile = $request->image;
+        if (!is_null($imageFile) && $imageFile->isValid()) {
+            Storage::putFile('public/shops', $imageFile);
+        }
 
+        return redirect()->route('owner.shops.index');
     }
 }
--- routes/owner.php
+++ routes/owner.php
@@ -35,7 +35,7 @@ Route::prefix('shops')
     ->group(function () {
         Route::get('index', [ShopController::class, 'index'])->name('shops.index');
         Route::get('edit/{shop}', [ShopController::class, 'edit'])->name('shops.edit');
-        Route::get('update/{shop}', [ShopController::class, 'update'])->name('shops.update');
+        Route::post('update/{shop}', [ShopController::class, 'update'])->name('shops.update');
     });
 
 Route::get('/dashboard', function () {
