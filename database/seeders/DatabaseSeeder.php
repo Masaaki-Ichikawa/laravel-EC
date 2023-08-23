31b04318137db140649e7c3b89620a4152c099dc
Masaaki-Ichikawa <matyaakitabasko@gmail.com>
2023-08-23 17:15:56 +0900

    stock

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -26,6 +26,7 @@ class DatabaseSeeder extends Seeder
             ImageSeeder::class,
             CategorySeeder::class,
             ProductSeeder::class,
+            StockSeeder::class,
         ]);
     }
 }
